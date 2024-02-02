8fb632d0d9184bf738d2e9e6bdaa3c2a24ec2645
snigdha devi <snigdhadevi212@gmail.com>
2024-02-02 09:31:59 +0000

    server.js updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const persons = require("./routes/api/persons");
  
-//connecting to MONGODB
+//Connecting to MONGODB
 //connectDB();
 
 
@@ -11,13 +11,13 @@ const persons = require("./routes/api/persons");
 //Installing the app
 const app = express();
 
-// enabling CORS here for safety of access of info from other domain
+// Enabling CORS here for safety of access of info from other domain
 app.use(cors());
 // handle JSON parsing properly
 app.use(express.json({
     extended: false
 }))
-//use4 the api grp instead of multiple paths for mul routes
+//Use the api grp instead of multiple paths for multiple routes
 app.use("/api/persons",persons)
 const port = process.env.PORT || 5000
 app.listen(port,() =>
