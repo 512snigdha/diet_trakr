de353fe0dad4425a45e9770a96c0b4b73eb04253
512snigdha <156826817+512snigdha@users.noreply.github.com>
2024-02-19 22:20:20 +0530

    Update Homepage.js

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -14,7 +14,7 @@ const Home = (props) => {
           <div className='col-md-8 m-auto web-desc'>
             <h1 className='display-5 text-center heading'>DIET TRACKER</h1>
             <h6 className='lead text-center'>Welcome To Our Website...!</h6>
-            <p className='lead text-center'>This can help the person for maintaining thir diet schedule</p>
+            <p className='lead text-center'>This can help the person for maintaining their diet schedule</p>
           </div>
         </div>
       </div>
